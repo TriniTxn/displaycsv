3d1fee8eeb7a60e660473d63cc43d306d76e937f
TriniTxn <giovanni.sgomes@gmail.com>
2024-09-19 16:23:40 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, render_template
+import pandas as pd
+app = Flask(__name__)
+@app.route('/')
+def index():
+    return render_template('index.html')
+@app.route('/display', methods=['POST'])
+def display_file():
+    file = request.files['file']
+    if not file:
+        return "No file"
+
+    try:
+        df = pd.read_csv(file, encoding='latin1', delimiter=";")
+    except UnicodeDecodeError:
+        file.seek(0)
+        try:
+            df = pd.read_csv(file, encoding='ISO-8859-1', delimiter=";")
+
+    return render_template('display.html', tables=[df.to_html(classes='data')], titles=df.columns.values)
+if __name__ == '__main__':
+    app.run(debug=True)
